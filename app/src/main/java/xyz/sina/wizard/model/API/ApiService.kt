f630ecb3dc178bac8c8996dcbc0e68215bdf8eda
Sina <sinaa.m1383@gmail.com>
2024-07-18 12:41:37 +0330

    update : change logo https://api.api-ninjas.com/v1/quotes?category=happiness

--- app/src/main/java/xyz/sina/wizard/model/API/ApiService.kt
+++ app/src/main/java/xyz/sina/wizard/model/API/ApiService.kt
@@ -6,13 +6,15 @@ import retrofit2.http.GET
 import xyz.sina.wizard.model.API.Json.Wise
 
 interface ApiService {
-    // fix this shit from API
-    @GET("random")
-    suspend fun getWise() : Wise
+
+    @GET("quotes/random")
+
+    suspend fun getWise() : Wise?
 
 
     companion object{
         private const val BASE_URL = "https://api.quotable.io/"
+
 
         fun create(): ApiService {
             return Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build().create(
