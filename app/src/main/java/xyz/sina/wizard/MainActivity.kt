b2994f0df11eec9cdee9f869ef2833647e2d257f
Sina <sinaa.m1383@gmail.com>
2024-07-21 12:47:47 +0330

    update: Hilt

--- app/src/main/java/xyz/sina/wizard/MainActivity.kt
+++ app/src/main/java/xyz/sina/wizard/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import xyz.sina.wizard.ui.theme.WizardTheme
 import xyz.sina.wizard.view.WiseView
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/xyz/sina/wizard/model/DI/Injection.kt
+++ app/src/main/java/xyz/sina/wizard/model/DI/Injection.kt
@@ -3,9 +3,9 @@ package xyz.sina.wizard.model.DI
 import xyz.sina.wizard.model.API.ApiService
 import xyz.sina.wizard.model.API.WiseRepo
 import xyz.sina.wizard.viewModel.WiseViewModel
+import javax.inject.Inject
 
 object Injection {
-    // TODO : use hilt
     fun provideWiseViewModel(): WiseViewModel {
         val apiService = ApiService.create()
         val repository = WiseRepo(apiService)
